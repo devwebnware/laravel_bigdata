650cd60e5ec9b7e8f75c0c3e138ff4b9dd6db334
unknown <devwebnware@gmail.com>
2024-03-28 20:03:25 +0530

    added seeders

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    public function run()
+    {
+        $this->call(RoleSeeder::class);
+        $this->call(UserSeeder::class);
+        $this->call(TagsSeeder::class);
+        $this->call(CategoriesSeeder::class);
+    }
+}
---
+++ database/seeders/TagsSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class TagsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('tags')->insert([
+            ['name' => 'Default Tag', 'created_by' => 1, 'created_at' => now(), 'updated_at' => now()],
+        ]);
+    }
+}
