32d129bec5371035d3c90d693ed33703e0e9601d
unknown <devwebnware@gmail.com>
2024-03-29 20:35:18 +0530

    added dashboard link and updated icons

--- resources/views/backend/layouts/partials/sidebar.blade.php
+++ resources/views/backend/layouts/partials/sidebar.blade.php
@@ -1,14 +1,11 @@
 <div class="nk-sidebar nk-sidebar-fixed is-dark " data-content="sidebarMenu">
     <div class="nk-sidebar-element nk-sidebar-head">
         <div class="nk-menu-trigger">
-            <a href="javascript:void(0)" class="nk-nav-toggle nk-quick-nav-icon d-xl-none" data-target="sidebarMenu"><em
-                    class="icon ni ni-arrow-left"></em></a>
-            <a href="javascript:void(0)" class="nk-nav-compact nk-quick-nav-icon d-none d-xl-inline-flex"
-                data-target="sidebarMenu"><em class="icon ni ni-menu"></em></a>
+            <a href="javascript:void(0)" class="nk-nav-toggle nk-quick-nav-icon d-xl-none" data-target="sidebarMenu"><em class="icon ni ni-arrow-left"></em></a>
+            <a href="javascript:void(0)" class="nk-nav-compact nk-quick-nav-icon d-none d-xl-inline-flex" data-target="sidebarMenu"><em class="icon ni ni-menu"></em></a>
         </div>
         <div class="nk-sidebar-brand">
-            <a href="{{ route('dashboard') }}"><img src="{{ asset('assets/images/admin-dashboard-logo.png') }}"
-                    class="w-75" alt="Admin Dashboard"></a>
+            <a href="{{ route('dashboard') }}"><em class="icon ni ni-linux" style="color: green; font-size: 30px"></em></a>
         </div>
     </div>
     <div class="nk-sidebar-element nk-sidebar-body">
@@ -16,24 +13,26 @@
             <div class="nk-sidebar-menu" data-simplebar>
                 <ul class="nk-menu">
                     @hasrole('admin')
-                            <li class="nk-menu-item has-sub">
-                                <a href="{{ route('categories.index') }}" class="nk-menu-link"><span
-                                        class="nk-menu-icon"><em class="icon ni ni-book"></em></span>
-                                    <span class="nk-menu-text">Category</span>
-                                </a>
-                            </li>
-                            <li class="nk-menu-item has-sub">
-                                <a href="{{ route('tags.index') }}" class="nk-menu-link"><span
-                                        class="nk-menu-icon"><em class="icon ni ni-book"></em></span>
-                                    <span class="nk-menu-text">Tag</span>
-                                </a>
-                            </li>
-                            <li class="nk-menu-item has-sub">
-                                <a href="{{ route('listings.index') }}" class="nk-menu-link"><span
-                                        class="nk-menu-icon"><em class="icon ni ni-book"></em></span>
-                                    <span class="nk-menu-text">Listing</span>
-                                </a>
-                            </li>
+                    <li class="nk-menu-item has-sub">
+                        <a href="{{ route('dashboard') }}" class="nk-menu-link"><span class="nk-menu-icon"><em class="icon ni ni-dashboard"></em></span>
+                            <span class="nk-menu-text">Dashboard</span>
+                        </a>
+                    </li>
+                    <li class="nk-menu-item has-sub">
+                        <a href="{{ route('categories.index') }}" class="nk-menu-link"><span class="nk-menu-icon"><em class="icon ni ni-book"></em></span>
+                            <span class="nk-menu-text">Category</span>
+                        </a>
+                    </li>
+                    <li class="nk-menu-item has-sub">
+                        <a href="{{ route('tags.index') }}" class="nk-menu-link"><span class="nk-menu-icon"><em class="icon ni ni-tag-alt"></em></span>
+                            <span class="nk-menu-text">Tag</span>
+                        </a>
+                    </li>
+                    <li class="nk-menu-item has-sub">
+                        <a href="{{ route('listings.index') }}" class="nk-menu-link"><span class="nk-menu-icon"><em class="icon ni ni-view-list-fill"></em></span>
+                            <span class="nk-menu-text">Listing</span>
+                        </a>
+                    </li>
                     @endcan
             </div>
         </div>
