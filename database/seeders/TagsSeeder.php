ef2ec2d2a0398fa0770eda498f976217927fd88f
unknown <devwebnware@gmail.com>
2024-03-29 20:36:44 +0530

    Updated tag seeder

--- database/seeders/TagsSeeder.php
+++ database/seeders/TagsSeeder.php
@@ -14,7 +14,7 @@ class TagsSeeder extends Seeder
     public function run(): void
     {
         DB::table('tags')->insert([
-            ['name' => 'Default Tag', 'created_by' => 1, 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'Default Tag', 'created_by' => 1, 'bg_color' => '#11DFB6', 'color'=> '#000000', 'created_at' => now(), 'updated_at' => now()],
         ]);
     }
 }
