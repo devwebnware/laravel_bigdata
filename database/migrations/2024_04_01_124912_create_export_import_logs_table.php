aca82237153d4428c7f597919ce0aa9de2b284f3
unknown <devwebnware@gmail.com>
2024-04-01 13:08:08 +0530

    Added model and migration for export import logs

---
+++ database/migrations/2024_04_01_124912_create_export_import_logs_table.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('export_import_logs', function (Blueprint $table) {
+            $table->id();
+            $table->boolean('type');
+            $table->foreignId('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('export_import_logs');
+    }
+};
