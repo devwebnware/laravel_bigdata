8c2d35b081f3f4b58aa17f4bf6237b54862b44cd
devwebnware <140055125+devwebnware@users.noreply.github.com>
2024-04-04 20:40:13 +0530

    Updated table style

--- resources/views/backend/listings/show.blade.php
+++ resources/views/backend/listings/show.blade.php
@@ -5,15 +5,15 @@
 <div class="card card-bordered mt-3 card-preview">
     <div class="p-5">
         <table class="table">
-            <thead>
+            <thead class="thead-dark">
                 <tr>
-                    <td colspan="2" class="text-center"><h3>{{ $listing->name }}</h3></td>
+                    <th scope="col" colspan="2" class="text-center"><h3 style="color: white">{{ $listing->name }}</h3></th>
                 </tr>
             </thead>
             <tbody>
                 <tr>
-                    <td>TAGS :</td>
-                    <td>
+                    <td scope="row">TAGS :</td>
+                    <td scope="row">
                     @forelse($listing->listingTags as $tag)
                     <span class="badge rounded-pill" style="background-color: {{ $tag->tagName->bg_color ?? 'default_color' }}; color: {{ $tag->tagName->color ?? 'color' }} ">
                         {{ substr($tag->tagName->name, 0, 20) }}
@@ -25,8 +25,8 @@
                 </tr>
                 @foreach($listing->getAttributes() as $key => $value)
                 <tr>
-                    <td>{{ strtoupper($key) }} :</td>
-                    <td>@if($value){{ $value }}@else N/A @endif</td>
+                    <td scope="row">{{ strtoupper($key) }} :</td>
+                    <td scope="row">@if($value){{ $value }}@else N/A @endif</td>
                 </tr>
                 @endforeach
             </tbody>
