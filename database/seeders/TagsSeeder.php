5f50b8a10d494ad0ccd91372134f4567c2565f2b
devwebnware <140055125+devwebnware@users.noreply.github.com>
2024-04-12 11:47:34 +0530

    Added 1 extra tag to seeder

--- database/seeders/TagsSeeder.php
+++ database/seeders/TagsSeeder.php
@@ -14,7 +14,8 @@ class TagsSeeder extends Seeder
     public function run(): void
     {
         DB::table('tags')->insert([
-            ['name' => 'Default Tag', 'created_by' => 1, 'bg_color' => '#11DFB6', 'color'=> '#000000', 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'NA', 'created_by' => 1, 'bg_color' => '#11DFB6', 'color'=> '#000000', 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'Dispatch', 'created_by' => 1, 'bg_color' => '#11DFB6', 'color'=> '#000000', 'created_at' => now(), 'updated_at' => now()],
         ]);
     }
 }
