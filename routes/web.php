0bceaaa37b53449e5529e42fa8eef873653f1bc6
sourabhongit <get.sourabhdas@gmail.com>
2024-04-18 01:46:03 +0530

    Removed unnecessary routes

--- routes/web.php
+++ routes/web.php
@@ -34,31 +34,12 @@ Route::group(['middleware' => ['auth']], function () {
     Route::get('permission/create', [PermissionController::class, 'create'])->name('permission.create');
     Route::post('permission/store', [PermissionController::class, 'store'])->name('permission.store');
     Route::get('permission/{id}/edit', [PermissionController::class, 'edit'])->name('permission.edit');
-    Route::get('performance/report/view', [DashboardController::class, 'kpiReportView'])->name('employee.kpiView');
     Route::patch('permission/{id}', [PermissionController::class, 'update'])->name('permission.update');
-    Route::get('earned/badges', [DashboardController::class, 'assignedBadges'])->name('assignedBadges.show');
-    Route::get('badge/{id}/terms-and-conditions', [DashboardController::class, 'badgeTermsAndConditions'])->name('badge.termsAndConditions');
-    Route::get('salary-display', [DashboardController::class, 'salaryDisplay'])->name('salary.display');
-    Route::get('salary/{id}/download', [DashboardController::class, 'downloadSalaryPDF'])->name('salary.downloadPdf');
-    Route::get('salary/{id}/print', [DashboardController::class, 'printSalaryPdf'])->name('salary.printPdf');
 
     // Flag View
-    Route::get('flags/earned', [DashboardController::class, 'flagEarned'])->name('flag.earned');
     Route::post('user-config/update', [DashboardController::class, 'configUpdate'])->name('user.configUpdate');
     Route::post('user/change/password', [DashboardController::class, 'changePassword'])->name('user.changePassword');
-    Route::get('asset/assigned', [DashboardController::class, 'assignedAssets'])->name('assignedAssets');
-    Route::post('asset/consumed', [DashboardController::class, 'markConsumed'])->name('markAssetConsumed');
 
-    // Guideline Routes
-    Route::get('guidelines', [DashboardController::class, 'guideline'])->name('guideline.index');
-    Route::get('guideline/{slug}', [DashboardController::class, 'viewGuidelines'])->name('guideline.view');
-    Route::get('assignedBadge/{id}', [DashboardController::class, 'assignedBadges'])->name('assignedBadge.view');
-
-    Route::get('/activity/logs', [DashboardController::class, 'activityLog'])->name('activityLog');
-    Route::get('notice/view', [DashboardController::class, 'viewNotice'])->name('notice.show');
-    Route::get('reminders', [DashboardController::class, 'salesReminder'])->name('reminder.all');
-    Route::get('sales/report', [DashboardController::class, 'salesReport'])->name('sales.report');
-    Route::post('sales/report/filter', [DashboardController::class, 'filterSalesReport'])->name('sales.filterReport');
     Route::get('dashboard', [DashboardController::class, 'dashboard'])->name('dashboard');
     Route::get('/register', [RegisteredUserController::class, 'create'])->name('register');
 
@@ -71,7 +52,7 @@ Route::group(['middleware' => ['auth']], function () {
     // Tags CRUD
     Route::resource('tags', TagController::class);
 
-    Route::controller(ListingController::class)->name('listings.')->prefix('listings/')->group(function() {
+    Route::controller(ListingController::class)->name('listings.')->prefix('listings/')->group(function () {
         // Listing CRUD
         Route::get('view', 'index')->name('index');
         Route::post('store', 'store')->name('store');
@@ -90,13 +71,13 @@ Route::group(['middleware' => ['auth']], function () {
         Route::get('data/import', 'import')->name('data.import');   // Import page view
         Route::get('import/columns', 'getColumns')->name('import.columns');
         Route::post('filter/export', 'exportFilter')->name('export.filtered');
-        Route::get('import/data/status', 'getStatus')->name('import.data.status');  
+        Route::get('import/data/status', 'getStatus')->name('import.data.status');
         Route::get('data/handel/export', 'handelExport')->name('data.handel.export');
         Route::post('data/handel/import', 'handelImport')->name('data.handel.import');
     });
 
     // Custom Fields Operations
-    Route::controller(FieldController::class)->name('field.')->group(function() {
+    Route::controller(FieldController::class)->name('field.')->group(function () {
         Route::get('fields', 'index')->name('index');
         Route::get('field/create', 'create')->name('create');
         Route::post('fields/store', 'store')->name('store');
