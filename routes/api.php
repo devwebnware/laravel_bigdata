bc7b39f45fd254c39a9990c77e9e406f53af58a1
sourabhongit <get.sourabhdas@gmail.com>
2024-04-22 23:13:44 +0530

    Removed unnecessary api rote info and controllers

--- routes/api.php
+++ routes/api.php
@@ -1,24 +1,8 @@
 <?php
 
-use App\Http\Controllers\Api\ClientController;
-use App\Http\Controllers\Api\ClientTicketController;
-use App\Http\Controllers\Api\HomeController;
-use App\Http\Controllers\Api\InvoiceController;
-use App\Http\Controllers\Api\ProjectController;
-use App\Http\Controllers\FrontendController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
