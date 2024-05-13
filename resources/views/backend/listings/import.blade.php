34be5f99b1463473a9ba5d68bd5b38d04dee00e6
sourabhongit <get.sourabhdas@gmail.com>
2024-05-14 00:50:33 +0530

    Added a loader

--- resources/views/backend/listings/import.blade.php
+++ resources/views/backend/listings/import.blade.php
@@ -17,11 +17,14 @@
             <input type="file" id="file" name='data' />
         </div>
         <div class="d-flex flex-direction-row">
-            <button type="submit" class="btn btn-primary" class="btn btn-sm btn-success">UPLOAD</button>
+            <button type="submit" class="btn btn-primary" onclick="uploadFile()" class="btn btn-sm btn-success">UPLOAD</button>
             <a href="{{ route('listings.index') }}" class="btn ml-2 btn-sm btn-secondary">CANCEL</a>
             <div class="spinner-border first-loader ml-3 d-none" role="status">
                 <span class="sr-only">Loading...</span>
             </div>
+            <progress id="progressBar" value="0" max="100" style="width:300px;"></progress>
+            <h3 id="status"></h3>
+            <p id="loaded_n_total"></p>
         </div>
     </form>
 </div>
@@ -196,6 +199,46 @@
             $('.form-control, .form-select').val('').trigger('change');
         });
     });
+</script>
+<script>
+    function _(el) {
+  return document.getElementById(el);
+}
+
+function uploadFile() {
+  var file = _("file").files[0];
+  // alert(file.name+" | "+file.size+" | "+file.type);
+  var formdata = new FormData();
+  formdata.append("file", file);
+  var ajax = new XMLHttpRequest();
+  ajax.upload.addEventListener("progress", progressHandler, false);
+  ajax.addEventListener("load", completeHandler, false);
+  ajax.addEventListener("error", errorHandler, false);
+  ajax.addEventListener("abort", abortHandler, false);
+  ajax.open("POST", "file_upload_parser.php");
+  //use file_upload_parser.php from above url
+  ajax.send(formdata);
+}
+
+function progressHandler(event) {
+  _("loaded_n_total").innerHTML = "Uploaded " + event.loaded + " bytes of " + event.total;
+  var percent = (event.loaded / event.total) * 100;
+  _("progressBar").value = Math.round(percent);
+  _("status").innerHTML = Math.round(percent) + "% uploaded... please wait";
+}
+
+function completeHandler(event) {
+  _("status").innerHTML = event.target.responseText;
+  _("progressBar").value = 0; //wil clear progress bar after successful upload
+}
+
+function errorHandler(event) {
+  _("status").innerHTML = "Upload Failed";
+}
+
+function abortHandler(event) {
+  _("status").innerHTML = "Upload Aborted";
+}
 </script>
 @endpush
 @endsection
